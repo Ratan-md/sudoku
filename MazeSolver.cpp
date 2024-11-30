5a5b33c1945ccf1c6c9066fd9dc1e4dd29636523
Ratan-md <138157867+Ratan-md@users.noreply.github.com>
2024-11-30 21:14:02 +0530

    Update MazeSolver.cpp

--- MazeSolver.cpp
+++ MazeSolver.cpp
@@ -1,4 +1,3 @@
-
 #include <iostream>
 #include <vector>
 #include <queue>
@@ -8,39 +7,42 @@
 #include <random>
 #include <ctime>
 
-const int INF = std::numeric_limits<int>::max();
+using namespace std;
+
+const int INF = numeric_limits<int>::max();
 
 class Maze {
 public:
-    std::vector<std::vector<char>> grid;
+    vector<vector<char>> grid;
     int rows, cols;
 
-    Maze(int r, int c) : rows(r), cols(c), grid(r, std::vector<char>(c, '1')) {}
+    Maze(int r, int c) : rows(r), cols(c), grid(r, vector<char>(c, '1')) {}
 
     void generateMaze() {
-        std::srand(std::time(nullptr));
+        srand(time(nullptr));
         carvePassage(0, 0);
-        grid[0][0] = '0'; // Start
-        grid[rows - 1][cols - 1] = '0'; // End
+        grid[0][0] = '0';           // Start point
+        grid[rows - 1][cols - 1] = '0'; // End point
     }
 
     void displayMaze() const {
-        for (const auto &row : grid) {
-            for (const auto &cell : row) {
-                std::cout << cell << " ";
+        for (const auto& row : grid) {
+            for (const auto& cell : row) {
+                cout << cell << " ";
             }
-            std::cout << std::endl;
+            cout << endl;
         }
     }
 
 private:
     void carvePassage(int r, int c) {
-        static const std::vector<std::pair<int, int>> directions = { {0, 1}, {1, 0}, {0, -1}, {-1, 0} };
-        std::vector<std::pair<int, int>> shuffled_directions = directions;
-        std::random_shuffle(shuffled_directions.begin(), shuffled_directions.end());
+        static const vector<pair<int, int>> directions = { {0, 1}, {1, 0}, {0, -1}, {-1, 0} };
+        vector<pair<int, int>> shuffled_directions = directions;
+        random_shuffle(shuffled_directions.begin(), shuffled_directions.end());
 
-        for (const auto &dir : shuffled_directions) {
-            int nr = r + dir.first * 2, nc = c + dir.second * 2;
+        for (const auto& dir : shuffled_directions) {
+            int nr = r + dir.first * 2;
+            int nc = c + dir.second * 2;
             if (isInBounds(nr, nc) && grid[nr][nc] == '1') {
                 grid[r + dir.first][c + dir.second] = '0';
                 grid[nr][nc] = '0';
@@ -56,36 +58,32 @@ private:
 
 class Graph {
 public:
-    std::vector<std::vector<std::pair<int, int>>> adjList;
+    vector<vector<pair<int, int>>> adjList;
     int nodes;
 
-    Graph(int numNodes) : nodes(numNodes) {
-        adjList.resize(numNodes);
-    }
+    Graph(int numNodes) : nodes(numNodes), adjList(numNodes) {}
 
     void addEdge(int u, int v, int weight) {
         adjList[u].emplace_back(v, weight);
     }
 
-    std::vector<int> dijkstra(int start) {
-        std::vector<int> dist(nodes, INF);
+    vector<int> dijkstra(int start, vector<int>& prev) {
+        vector<int> dist(nodes, INF);
         dist[start] = 0;
-        std::priority_queue<std::pair<int, int>, std::vector<std::pair<int, int>>, std::greater<>> pq;
+        prev[start] = -1;
+
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<>> pq;
         pq.emplace(0, start);
 
         while (!pq.empty()) {
             int u = pq.top().second;
-            int d = pq.top().first;
             pq.pop();
 
-            if (d != dist[u]) continue;
-
-            for (const auto &edge : adjList[u]) {
-                int v = edge.first;
-                int weight = edge.second;
-
+            for (const auto& edge : adjList[u]) {
+                int v = edge.first, weight = edge.second;
                 if (dist[u] + weight < dist[v]) {
                     dist[v] = dist[u] + weight;
+                    prev[v] = u;
                     pq.emplace(dist[v], v);
                 }
             }
@@ -94,12 +92,12 @@ public:
     }
 };
 
-std::vector<int> getPath(const std::vector<int> &dist, const std::vector<int> &prev, int target) {
-    std::vector<int> path;
+vector<int> getPath(const vector<int>& prev, int target) {
+    vector<int> path;
     for (int at = target; at != -1; at = prev[at]) {
         path.push_back(at);
     }
-    std::reverse(path.begin(), path.end());
+    reverse(path.begin(), path.end());
     return path;
 }
 
@@ -116,31 +114,31 @@ int main() {
 
     for (int i = 0; i < rows; ++i) {
         for (int j = 0; j < cols; ++j) {
-            if (maze.grid[i][j] == '1') continue; // Wall
-            if (i > 0 && maze.grid[i - 1][j] != '1') graph.addEdge(index(i, j), index(i - 1, j), 1);
-            if (i < rows - 1 && maze.grid[i + 1][j] != '1') graph.addEdge(index(i, j), index(i + 1, j), 1);
-            if (j > 0 && maze.grid[i][j - 1] != '1') graph.addEdge(index(i, j), index(i, j - 1), 1);
-            if (j < cols - 1 && maze.grid[i][j + 1] != '1') graph.addEdge(index(i, j), index(i, j + 1), 1);
+            if (maze.grid[i][j] == '1') continue; // Skip walls
+            if (i > 0 && maze.grid[i - 1][j] == '0') graph.addEdge(index(i, j), index(i - 1, j), 1);
+            if (i < rows - 1 && maze.grid[i + 1][j] == '0') graph.addEdge(index(i, j), index(i + 1, j), 1);
+            if (j > 0 && maze.grid[i][j - 1] == '0') graph.addEdge(index(i, j), index(i, j - 1), 1);
+            if (j < cols - 1 && maze.grid[i][j + 1] == '0') graph.addEdge(index(i, j), index(i, j + 1), 1);
         }
     }
 
-    std::vector<int> dist = graph.dijkstra(start);
+    vector<int> prev(rows * cols, -1);
+    vector<int> dist = graph.dijkstra(start, prev);
 
     if (dist[end] == INF) {
-        std::cout << "No path found from start to end." << std::endl;
+        cout << "No path found from start to end." << endl;
         return 1;
     }
 
-    std::vector<int> prev(rows * cols, -1);
-    std::vector<int> path = getPath(dist, prev, end);
+    vector<int> path = getPath(prev, end);
 
-    std::cout << "Shortest path from start to end:" << std::endl;
+    cout << "Shortest path from start to end:" << endl;
     for (int node : path) {
         int row = node / cols;
         int col = node % cols;
-        std::cout << "(" << row << ", " << col << ") ";
+        cout << "(" << row << ", " << col << ") ";
     }
-    std::cout << std::endl;
+    cout << endl;
 
     return 0;
 }
